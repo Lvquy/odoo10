8f36fbcbb845b3f5c4a8b47c2210bcb56339709f
Lvquy <quyseo.ictu@gmail.com>
2019-04-04 15:43:46 +0700

    myaddons_git

---
+++ sms_birthday/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Auto SMS Birthday",
+    "summary":"",
+    "author":"Lv Quy",
+    "license":'LGPL-3',
+    "description":'''Push sms''',
+    "depends":['base','hr'],
+    "data":[
+        # 'view.xml'
+        'data/data.xml'
+    ],
+    'installable': True,
+    'application'   : True,
+    'auto_install'  : False,
+}
---
+++ sms_birthday/models/model.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+from datetime import date
+
+import dateutil.parser
+from pushbullet import Pushbullet
+
+from odoo import models, api
+
+API_KEY ='o.08l11dgmJ0Et9LZRJei5EPUJ6onmAmfL'
+pb = Pushbullet(API_KEY)
+device = pb.devices[0]
+
+class Birthday(models.Model):
+    _inherit = 'hr.employee'
+
+    def auto_sms(self):
+        data = self.env['hr.employee']
+        id_all = data.search([])
+
+        # id_need = data.browse(id_all)
+        # print id_need
+        for i in id_all:
+            day = date.today().day
+            month = date.today().month
+
+            birthday = i.birthday
+            birthday_day = dateutil.parser.parse(birthday).day
+            birthday_month = dateutil.parser.parse(birthday).month
+            if birthday_month == month and birthday_day == day:
+                id_need = data.browse(i.id)
+                sms = "Chúc bạn " + str(id_need.name) + " Sinh nhật vui vẻ! " + str(birthday)
+                pb.push_note('Happy Birthday!', sms)
