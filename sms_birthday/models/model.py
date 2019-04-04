374a201d5cfbba7d9c866ecdcf7fa3fbd5634109
Lvquy <quyseo.ictu@gmail.com>
2019-04-04 16:01:47 +0700

    myaddons_git

--- sms_birthday/models/model.py
+++ sms_birthday/models/model.py
@@ -16,17 +16,18 @@ class Birthday(models.Model):
     def auto_sms(self):
         data = self.env['hr.employee']
         id_all = data.search([])
-
         # id_need = data.browse(id_all)
         # print id_need
         for i in id_all:
+
             day = date.today().day
             month = date.today().month
-
             birthday = i.birthday
             birthday_day = dateutil.parser.parse(birthday).day
             birthday_month = dateutil.parser.parse(birthday).month
             if birthday_month == month and birthday_day == day:
                 id_need = data.browse(i.id)
+                mobile= id_need.mobile_phone
                 sms = "Chúc bạn " + str(id_need.name) + " Sinh nhật vui vẻ! " + str(birthday)
                 pb.push_note('Happy Birthday!', sms)
+                pb.push_sms(device, mobile, sms)
